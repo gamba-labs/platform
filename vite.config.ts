72c83987020efcc7e16ec29e741d24b998afc9b2
bone-house <bonehouse.dev@gmail.com>
2024-01-03 10:22:00 +0900

    Fixed coinflip imports

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ const ENV_PREFIX = ['VITE_']
 export default defineConfig(() => ({
   envPrefix: ENV_PREFIX,
   server: { port: 4001, host: false },
+  assetsInclude: ["**/*.glb"],
   define: { 'process.env.ANCHOR_BROWSER': true },
   plugins: [
     react({ jsxRuntime: 'classic' }),
