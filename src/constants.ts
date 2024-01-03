3e52a3d3c6225133a0cd456e37cf925ef211e461
bone-house <bonehouse.dev@gmail.com>
2024-01-03 10:00:26 +0900

    Default rpc endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,12 @@
-import { clusterApiUrl } from '@solana/web3.js'
 import { GambaStandardTokens, TokenMeta } from 'gamba-react-ui-v2'
 
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? clusterApiUrl()
+// Can be configured in .env
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://api.mainnet-beta.solana.com"
 
+// Change this value to your Solana address
 export const PLATFORM_CREATOR_ADDRESS = 'V2grJiwjs25iJYqumbHyKo5MTK7SFqZSdmoRaj8QWb9'
 
-/** Appears in ShareModal */
+// Appears in ShareModal
 export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
 
 // List of tokens supported by this platform
@@ -14,8 +15,8 @@ export const TOKENS: TokenMeta[] = [
   GambaStandardTokens.usdc,
   // {
   //   mint: new PublicKey(""),
-  //   symbol: '?',
-  //   name: '?',
+  //   symbol: '???',
+  //   name: 'Custom SPL Token',
   //   decimals: 1e9,
   //   baseWager: 1,
   // }
