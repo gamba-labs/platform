4b113f369d8ab20c104ad07e2d56b8744d703f31
Prosper <nkonyeprospero@gmail.com>
2024-01-03 02:28:03 +0100

    first push

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,28 +1,18 @@
-import React from 'react'
-import { Button } from '../components/Button'
-import { Section } from '../components/Section'
-import { Icon } from '../components/Icon'
-import styles from './Home.module.css'
+import React from "react";
+import { Button } from "../components/Button";
+import { Section } from "../components/Section";
+import { Icon } from "../components/Icon";
+import styles from "./Home.module.css";
 
 export function Home() {
   return (
     <div className={styles.banner}>
       <Section>
-        <h2>
-          Welcome to Solbets!
-        </h2>
-        <div style={{ display: 'flex', gap: '10px' }}>
-          {/* <Button
-            as="a"
-            color="white"
-            href="https://gamba.so"
-            target="_blank"
-            icon={<Icon.ExternalLink />}
-          >
-            Learn more
-          </Button> */}
-        </div>
+        {/* <h2>Welcome to Solbets!</h2> */}
+        {/* <div style={{ display: 'flex', gap: '10px' }}>
+
+        </div> */}
       </Section>
     </div>
-  )
+  );
 }
