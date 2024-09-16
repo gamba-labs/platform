f2bc43c06d9c25a8613da4ea1ac687ac282bdce4
skibbidibet <brandonblumenkraft@gmail.com>
2024-09-16 16:24:59 +0300

    Update RecentPlays.tsx

--- src/sections/RecentPlays/RecentPlays.tsx
+++ src/sections/RecentPlays/RecentPlays.tsx
@@ -91,7 +91,7 @@ export default function RecentPlays() {
         ),
       )}
       <GambaUi.Button main onClick={() => window.open(`${EXPLORER_URL}/platform/${PLATFORM_CREATOR_ADDRESS.toString()}`)}>
-        🚀 Explorer
+        🚀 Liquidity Explorer (SOON)
       </GambaUi.Button>
     </Container>
   )
