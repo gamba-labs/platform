8ce697f4bd36ee918da17a6093de53ef06ec9849
”Berhawu,Redwan” <inf4198@hs-worms.de>
2025-04-09 20:46:04 +0200

    changes

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -115,7 +115,7 @@ export function WelcomeBanner() {
   return (
     <Welcome>
       <div>
-        <h1>Welcome to Gamba v2 👋</h1>
+        <h1>Welcome to CryptoCasinoClub 👋</h1>
         <p>
           A fair, simple and decentralized casino on Solana.
         </p>
