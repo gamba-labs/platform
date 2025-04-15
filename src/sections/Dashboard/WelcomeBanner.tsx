db83620ab247c755e7710f854f9f95c277ce29e7
banabets <gummy-penal-0o@icloud.com>
2025-04-15 03:21:11 -0400

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -61,7 +61,7 @@ const Welcome = styled.div`
     }
   }
 
-  background-image: url(https://iili.io/3cT9XgR.png;); 
+  background-image: url(https://iili.io/309S6o7.png;); 
   background-size: cover; 
  background-position: auto;
   border-radius: 20px;
