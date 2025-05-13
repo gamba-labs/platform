3608b4865e92454bd73d17a0a6659287d6d3835f
Banabets <gummy-penal-0o@icloud.com>
2025-05-13 03:03:34 -0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -22,7 +22,7 @@ export const PLATFORM_CREATOR_FEE = 0.02 // 1% !!max 5%!!
 export const PLATFORM_JACKPOT_FEE = 0.002 // 0.1%
 
 // Referral fee (in %)
-export const PLATFORM_REFERRAL_FEE = 0.05 // 0.25%
+export const PLATFORM_REFERRAL_FEE = 0.1 // 0.25%
 
 /** If the user should be able to revoke an invite after they've accepted an invite */
 export const PLATFORM_ALLOW_REFERRER_REMOVAL = true
